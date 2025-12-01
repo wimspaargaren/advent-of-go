55e4696fd9576d3132cdfa658203887daccc50b2
Wim Spaargaren <wim_spaargaren@live.nl>
2025-12-01 06:48:30 +0100

    2025 day 1

--- 2025/day1/main.go
+++ 2025/day1/main.go
@@ -1,8 +1,63 @@
 package main
 
-import "github.com/wimspaargaren/aoc"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/wimspaargaren/aoc"
+)
+
+type Rotation struct {
+	Dir string
+	N   int
+}
 
 func main() {
-	input := aoc.MustReadFile("input.txt")
-	_ = input
+
+	lines := strings.Split(aoc.MustReadFile("input.txt"), "\n")
+	_ = lines
+	rotations := []Rotation{}
+	for _, line := range lines {
+		rotations = append(rotations, parseInput(line))
+	}
+	start := 50
+	totalZero := 0
+	// start is between 0 and 99
+	for _, rot := range rotations {
+		if rot.Dir == "R" {
+			for i := 0; i < rot.N; i++ {
+				start++
+				if start == 100 {
+					totalZero++
+					start = 0
+				}
+			}
+		} else {
+			for i := 0; i < rot.N; i++ {
+				start--
+				if start == 0 {
+					totalZero++
+				}
+				if start == -1 {
+					start = 99
+				}
+			}
+		}
+
+	}
+	fmt.Println(totalZero)
+}
+
+func parseInput(line string) Rotation {
+	dir := string(line[0])
+	n, err := strconv.Atoi(line[1:])
+	if err != nil {
+		panic(err)
+	}
+
+	return Rotation{
+		Dir: dir,
+		N:   n,
+	}
 }
