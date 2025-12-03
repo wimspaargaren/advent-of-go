260a3c878641d7114c81e007296caea9896dfbe0
Wim Spaargaren <wim_spaargaren@live.nl>
2025-12-03 07:52:50 +0100

    day 3 2025

--- 2025/day3/main.go
+++ 2025/day3/main.go
@@ -1,8 +1,52 @@
 package main
 
-import "github.com/wimspaargaren/aoc"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/wimspaargaren/aoc"
+)
 
 func main() {
 	input := aoc.MustReadFile("input.txt")
-	_ = input
+	lines := strings.Split(string(input), "\n")
+	part1 := 0
+	part2 := 0
+	for _, line := range lines {
+		numbers := []int{}
+		for _, char := range line {
+			n, err := strconv.Atoi(string(char))
+			if err != nil {
+				panic(err)
+			}
+			numbers = append(numbers, n)
+		}
+
+		part1 += largest("", 0, 2, numbers)
+		part2 += largest("", 0, 12, numbers)
+	}
+	fmt.Println("part 1:", part1)
+	fmt.Println("part 2:", part2)
+}
+
+func largest(total string, startIndex int, totalLength int, numbers []int) int {
+	index := 0
+	maxNumber := 0
+	for i := startIndex; i < len(numbers)-(totalLength-len(total)-1); i++ {
+		if numbers[i] > maxNumber {
+			maxNumber = numbers[i]
+			index = i + 1
+		}
+	}
+	total += strconv.Itoa(maxNumber)
+	if len(total) == totalLength {
+		n, err := strconv.Atoi(total)
+		if err != nil {
+			panic(err)
+		}
+		return n
+	}
+
+	return largest(total, index, totalLength, numbers)
 }
