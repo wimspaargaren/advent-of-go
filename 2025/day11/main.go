565ed1aaa87df28fc637879a01bbbaea2e1fc4bd
Wim Spaargaren <wim_spaargaren@live.nl>
2025-12-12 15:18:59 +0100

    day 11 2025

--- 2025/day11/main.go
+++ 2025/day11/main.go
@@ -1,8 +1,85 @@
 package main
 
-import "github.com/wimspaargaren/aoc"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/wimspaargaren/aoc"
+)
 
 func main() {
 	input := aoc.MustReadFile("input.txt")
-	_ = input
+	graph := parseInput(input)
+	part1 := part1("you", "out", graph, map[string]int{})
+	fmt.Println("Part 1:", part1)
+	start := "svr"
+	end := "out"
+	part2 := pathsTo(start, end, false, false, graph, map[node]int{})
+	fmt.Println("Part 2:", part2)
+}
+
+type node struct {
+	val    string
+	hasFFT bool
+	hasDAC bool
+}
+
+func part1(current string, target string, graph map[string][]string, visited map[string]int) int {
+	if current == target {
+		return 1
+	}
+
+	visitedCount, ok := visited[current]
+	if ok {
+		return visitedCount
+	}
+
+	next := graph[current]
+	total := 0
+	for _, n := range next {
+		total += part1(n, target, graph, visited)
+	}
+	visited[current] = total
+	return total
+}
+
+func pathsTo(current string, target string, hasFFt, hasDAC bool, graph map[string][]string, visited map[node]int) int {
+	if current == "fft" {
+		hasFFt = true
+	}
+	if current == "dac" {
+		hasDAC = true
+	}
+
+	if current == target {
+		if hasDAC && hasFFt {
+			return 1
+		}
+		return 0
+	}
+
+	visitedCount, ok := visited[node{val: current, hasFFT: hasFFt, hasDAC: hasDAC}]
+	if ok {
+		return visitedCount
+	}
+
+	next := graph[current]
+	total := 0
+	for _, n := range next {
+		total += pathsTo(n, target, hasFFt, hasDAC, graph, visited)
+	}
+	visited[node{val: current, hasFFT: hasFFt, hasDAC: hasDAC}] = total
+	return total
+}
+
+func parseInput(input string) map[string][]string {
+	res := map[string][]string{}
+	lines := strings.Split(strings.TrimSpace(input), "\n")
+	for _, line := range lines {
+		parts := strings.Split(line, ": ")
+		key := parts[0]
+		values := strings.Split(parts[1], " ")
+		res[key] = values
+	}
+	return res
 }
