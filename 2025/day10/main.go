315f487c3cfe661643e18c0fe21f0f8811929fc0
Wim Spaargaren <wim_spaargaren@live.nl>
2025-12-12 23:40:21 +0100

    day 10 2025 part 2

--- 2025/day10/main.go
+++ 2025/day10/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"slices"
+	"math"
 	"strconv"
 	"strings"
 
@@ -11,181 +11,237 @@ import (
 
 func main() {
 	input := aoc.MustReadFile("input.txt")
+
 	machines := []Machine{}
 	for _, line := range strings.Split(input, "\n") {
-		machine := parseMachine(line)
-		machines = append(machines, machine)
-	}
-	part1 := 0
-	for i, machine := range machines {
-		fmt.Println("Processing machine", i+1, "of", len(machines))
-		part1 += process(machine)
-	}
-	fmt.Println("Part 1:", part1)
-
-}
-
-type Set struct {
-	counter []int
-	presses []int
-}
-
-func NewSet(config []string) *Set {
-	return &Set{
-		counter: make([]int, len(config)),
-		presses: []int{},
-	}
-}
-
-func (s *Set) Add(buttons [][]int, press int) {
-	for _, index := range buttons[press] {
-		s.counter[index]++
-	}
-	s.presses = append(s.presses, press)
-	slices.Sort(s.presses)
-}
-
-func (s *Set) Clone() *Set {
-	newCounter := make([]int, len(s.counter))
-	copy(newCounter, s.counter)
-	newPresses := make([]int, len(s.presses))
-	copy(newPresses, s.presses)
-	return &Set{
-		counter: newCounter,
-		presses: newPresses,
-	}
-}
-
-func (s *Set) Check(config []string) bool {
-	for i, conf := range config {
-		if conf == "." {
-			s.counter[i] %= 2
-			if s.counter[i] != 0 {
-				return false
-			}
+		if strings.TrimSpace(line) == "" {
+			continue
 		}
-		if conf == "#" {
-			s.counter[i] %= 2
-			if s.counter[i] != 1 {
-				return false
-			}
+		machines = append(machines, parseMachine(line))
+	}
+
+	totalPresses := 0.0
+
+	for _, m := range machines {
+		totalPresses += solve(floatSlice(m.JoltageRequirements), m.Buttons)
+	}
+
+	fmt.Println("Part 2:", int(totalPresses))
+}
+
+func solve(joltageRequirements []float64, buttons [][]int) float64 {
+	numRows := len(joltageRequirements)
+	numCols := len(buttons)
+	matrix := matrix(joltageRequirements, buttons)
+
+	pivotCols := gaussianElimination(matrix, numRows, numCols)
+	printMatrix(matrix)
+
+	isPivot := make(map[int]bool)
+	pivotRowMap := make(map[int]int)
+	for r, c := range pivotCols {
+		if c != -1 {
+			isPivot[c] = true
+			pivotRowMap[c] = r
 		}
 	}
-	return true
+
+	var freeCols []int
+	for col := 0; col < numCols; col++ {
+		if !isPivot[col] {
+			freeCols = append(freeCols, col)
+		}
+	}
+
+	minTotal := math.MaxFloat64
+
+	maxSearchLimit := 0.0
+	for _, t := range joltageRequirements {
+		if t > maxSearchLimit {
+			maxSearchLimit = t
+		}
+	}
+
+	return search(matrix, maxSearchLimit, minTotal, numCols, numRows, isPivot, pivotRowMap, freeCols, 0, make(map[int]float64))
 }
 
-func process(machine Machine) int {
-	trackMap := make(map[string]bool)
-	initialSet := []*Set{}
-	for i := range machine.Buttons {
-		newSet := NewSet(machine.Config)
-		newSet.Add(machine.Buttons, i)
-		initialSet = append(initialSet, newSet)
-	}
-	for {
-		for _, set := range initialSet {
-			if set.Check(machine.Config) {
-				return len(set.presses)
-			}
-		}
-		initialSet = increaseSets(trackMap, initialSet, machine.Buttons)
-		if len(initialSet) == 0 {
-			panic("NOTHIGN FOUND!")
-		}
-	}
-	return -1
-}
+func matrix(joltageRequirements []float64, buttons [][]int) [][]float64 {
+	numRows := len(joltageRequirements)
+	numCols := len(buttons)
 
-// adds all combinations of button presses to the sets
-func increaseSets(trackMap map[string]bool, sets []*Set, buttons [][]int) []*Set {
-	newSets := []*Set{}
-	for _, set := range sets {
-		for i := range buttons {
-			clonedSet := set.Clone()
-			clonedSet.Add(buttons, i)
-			_, ok := trackMap[fmtIntSlice(clonedSet.presses)]
-			if ok {
-				continue
-			}
-			trackMap[fmtIntSlice(clonedSet.presses)] = true
-			newSets = append(newSets, clonedSet)
-		}
+	matrix := make([][]float64, numRows)
+	for i := range matrix {
+		matrix[i] = make([]float64, numCols+1)
+		matrix[i][numCols] = joltageRequirements[i]
 	}
-	return newSets
-}
 
-func fmtIntSlice(slice []int) string {
-	strs := []string{}
-	for _, num := range slice {
-		strs = append(strs, strconv.Itoa(num))
-	}
-	return strings.Join(strs, ",")
-}
-
-func checkSet(config []string, set []int, buttons [][]int) bool {
-	counter := make([]int, len(config))
-	for _, pres := range set {
-		for _, index := range buttons[pres] {
-			counter[index]++
-		}
-	}
-	for i, conf := range config {
-		if conf == "." {
-			counter[i] %= 2
-			if counter[i] != 0 {
-				return false
-			}
-		}
-		if conf == "#" {
-			counter[i] %= 2
-			if counter[i] != 1 {
-				return false
+	for col, btnIndices := range buttons {
+		for _, rowIdx := range btnIndices {
+			if rowIdx < numRows {
+				matrix[rowIdx][col] = 1.0
 			}
 		}
 	}
-	return true
+	return matrix
+}
+
+func search(matrix [][]float64, maxSearchLimit, minTotal float64, numCols, numRows int, isPivot map[int]bool, pivotRowMap map[int]int, freeCols []int, idx int, currentFreeVals map[int]float64) float64 {
+	if idx == len(freeCols) {
+		currentSolution := make(map[int]float64)
+		currentSum := 0.0
+
+		for col, val := range currentFreeVals {
+			currentSolution[col] = val
+			currentSum += val
+		}
+
+		isValid := true
+
+		for col := 0; col < numCols; col++ {
+			if isPivot[col] {
+				row := pivotRowMap[col]
+
+				sumFree := 0.0
+				for _, fCol := range freeCols {
+					sumFree += matrix[row][fCol] * currentFreeVals[fCol]
+				}
+
+				val := matrix[row][numCols] - sumFree
+
+				if val < -0.0001 {
+					isValid = false
+					break
+				}
+
+				nearest := math.Round(val)
+				if math.Abs(val-nearest) > 0.0001 {
+					isValid = false
+					break
+				}
+
+				currentSolution[col] = nearest
+				currentSum += nearest
+			}
+		}
+
+		if isValid {
+			if currentSum < minTotal {
+				minTotal = currentSum
+			}
+		}
+		return minTotal
+	}
+
+	fCol := freeCols[idx]
+
+	for val := 0.0; val <= maxSearchLimit; val++ {
+		if minTotal != math.MaxFloat64 && val > minTotal {
+			break
+		}
+
+		currentFreeVals[fCol] = val
+		temp := search(matrix, maxSearchLimit, minTotal, numCols, numRows, isPivot, pivotRowMap, freeCols, idx+1, currentFreeVals)
+		if temp < minTotal {
+			minTotal = temp
+		}
+	}
+	return minTotal
+}
+
+func gaussianElimination(m [][]float64, rows, cols int) []int {
+	pivotRow := 0
+	pivotCols := make([]int, rows)
+	for i := range pivotCols {
+		pivotCols[i] = -1
+	}
+
+	for col := 0; col < cols && pivotRow < rows; col++ {
+		selectedRow := pivotRow
+		// search for row with max value in this column
+		for i := pivotRow + 1; i < rows; i++ {
+			if math.Abs(m[i][col]) > math.Abs(m[selectedRow][col]) {
+				selectedRow = i
+			}
+		}
+
+		// ignore column if all values are zero
+		if math.Abs(m[selectedRow][col]) <= 0 {
+			continue
+		}
+
+		// swap rows
+		m[pivotRow], m[selectedRow] = m[selectedRow], m[pivotRow]
+		pivotCols[pivotRow] = col
+
+		div := m[pivotRow][col]
+		// divide pivot row by largest value
+		for j := col; j <= cols; j++ {
+			m[pivotRow][j] /= div
+		}
+
+		// eliminate other rows
+		for i := 0; i < rows; i++ {
+			if i != pivotRow {
+				mult := m[i][col]
+				for j := col; j <= cols; j++ {
+					m[i][j] -= mult * m[pivotRow][j]
+				}
+			}
+		}
+		pivotRow++
+	}
+	return pivotCols
 }
 
 func parseMachine(input string) Machine {
-	parts := strings.Split(input, " ")
-	config := parts[0]
-	jotlageRequirement := parts[len(parts)-1]
-	jotlageRequirement = jotlageRequirement[1 : len(jotlageRequirement)-1]
+	startBrace := strings.Index(input, "{")
+	endBrace := strings.Index(input, "}")
+	targetStr := input[startBrace+1 : endBrace]
+
 	joltages := []int{}
-	for _, s := range strings.Split(jotlageRequirement, ",") {
-		joltage, err := strconv.Atoi(s)
-		if err != nil {
-			panic(err)
-		}
-		joltages = append(joltages, joltage)
+	for _, s := range strings.Split(targetStr, ",") {
+		val, _ := strconv.Atoi(strings.TrimSpace(s))
+		joltages = append(joltages, val)
 	}
-	fmt.Println("JOLTAGE", joltages)
 
 	buttons := [][]int{}
-	for i := 1; i < len(parts)-1; i++ {
-		part := parts[i]
-		temp := part[1 : len(part)-1]
-		button := []int{}
-		for _, s := range strings.Split(temp, ",") {
-			index, err := strconv.Atoi(s)
-			if err != nil {
-				panic(err)
+	tokens := strings.Split(input, " ")
+	for _, t := range tokens {
+		if strings.HasPrefix(t, "(") && strings.HasSuffix(t, ")") {
+			content := t[1 : len(t)-1]
+			btn := []int{}
+			if len(content) > 0 {
+				for _, numStr := range strings.Split(content, ",") {
+					idx, _ := strconv.Atoi(strings.TrimSpace(numStr))
+					btn = append(btn, idx)
+				}
 			}
-			button = append(button, index)
+			buttons = append(buttons, btn)
 		}
-		buttons = append(buttons, button)
 	}
-	config = config[1 : len(config)-1]
 
 	return Machine{
-		Config:              strings.Split(config, ""),
-		JoltageRequirements: joltages,
 		Buttons:             buttons,
+		JoltageRequirements: joltages,
 	}
 }
 
 type Machine struct {
-	Config              []string
 	Buttons             [][]int
 	JoltageRequirements []int
+}
+
+func floatSlice(ints []int) []float64 {
+	floats := []float64{}
+	for _, i := range ints {
+		floats = append(floats, float64(i))
+	}
+	return floats
+}
+
+func printMatrix(m [][]float64) {
+	fmt.Println("------")
+	for _, row := range m {
+		fmt.Println(row)
+	}
 }
---
+++ 2025/day10/part1/main.go
@@ -0,0 +1,190 @@
+package main
+
+import (
+	"fmt"
+	"slices"
+	"strconv"
+	"strings"
+
+	"github.com/wimspaargaren/aoc"
+)
+
+func main() {
+	input := aoc.MustReadFile("input.txt")
+	machines := []Machine{}
+	for _, line := range strings.Split(input, "\n") {
+		machine := parseMachine(line)
+		machines = append(machines, machine)
+	}
+	part1 := 0
+	for i, machine := range machines {
+		fmt.Println("Processing machine", i+1, "of", len(machines))
+		part1 += process(machine)
+	}
+	fmt.Println("Part 1:", part1)
+
+}
+
+type Set struct {
+	counter []int
+	presses []int
+}
+
+func NewSet(config []string) *Set {
+	return &Set{
+		counter: make([]int, len(config)),
+		presses: []int{},
+	}
+}
+
+func (s *Set) Add(buttons [][]int, press int) {
+	for _, index := range buttons[press] {
+		s.counter[index]++
+	}
+	s.presses = append(s.presses, press)
+	slices.Sort(s.presses)
+}
+
+func (s *Set) Clone() *Set {
+	newCounter := make([]int, len(s.counter))
+	copy(newCounter, s.counter)
+	newPresses := make([]int, len(s.presses))
+	copy(newPresses, s.presses)
+	return &Set{
+		counter: newCounter,
+		presses: newPresses,
+	}
+}
+
+func (s *Set) Check(config []string) bool {
+	for i, conf := range config {
+		if conf == "." {
+			s.counter[i] %= 2
+			if s.counter[i] != 0 {
+				return false
+			}
+		}
+		if conf == "#" {
+			s.counter[i] %= 2
+			if s.counter[i] != 1 {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func process(machine Machine) int {
+	trackMap := make(map[string]bool)
+	initialSet := []*Set{}
+	for i := range machine.Buttons {
+		newSet := NewSet(machine.Config)
+		newSet.Add(machine.Buttons, i)
+		initialSet = append(initialSet, newSet)
+	}
+	for {
+		for _, set := range initialSet {
+			if set.Check(machine.Config) {
+				return len(set.presses)
+			}
+		}
+		initialSet = increaseSets(trackMap, initialSet, machine.Buttons)
+		if len(initialSet) == 0 {
+			panic("NOTHIGN FOUND!")
+		}
+	}
+}
+
+// adds all combinations of button presses to the sets
+func increaseSets(trackMap map[string]bool, sets []*Set, buttons [][]int) []*Set {
+	newSets := []*Set{}
+	for _, set := range sets {
+		for i := range buttons {
+			clonedSet := set.Clone()
+			clonedSet.Add(buttons, i)
+			_, ok := trackMap[fmtIntSlice(clonedSet.presses)]
+			if ok {
+				continue
+			}
+			trackMap[fmtIntSlice(clonedSet.presses)] = true
+			newSets = append(newSets, clonedSet)
+		}
+	}
+	return newSets
+}
+
+func fmtIntSlice(slice []int) string {
+	strs := []string{}
+	for _, num := range slice {
+		strs = append(strs, strconv.Itoa(num))
+	}
+	return strings.Join(strs, ",")
+}
+
+func checkSet(config []string, set []int, buttons [][]int) bool {
+	counter := make([]int, len(config))
+	for _, pres := range set {
+		for _, index := range buttons[pres] {
+			counter[index]++
+		}
+	}
+	for i, conf := range config {
+		if conf == "." {
+			counter[i] %= 2
+			if counter[i] != 0 {
+				return false
+			}
+		}
+		if conf == "#" {
+			counter[i] %= 2
+			if counter[i] != 1 {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func parseMachine(input string) Machine {
+	parts := strings.Split(input, " ")
+	config := parts[0]
+	jotlageRequirement := parts[len(parts)-1]
+	jotlageRequirement = jotlageRequirement[1 : len(jotlageRequirement)-1]
+	joltages := []int{}
+	for _, s := range strings.Split(jotlageRequirement, ",") {
+		joltage, err := strconv.Atoi(s)
+		if err != nil {
+			panic(err)
+		}
+		joltages = append(joltages, joltage)
+	}
+	fmt.Println("JOLTAGE", joltages)
+
+	buttons := [][]int{}
+	for i := 1; i < len(parts)-1; i++ {
+		part := parts[i]
+		temp := part[1 : len(part)-1]
+		button := []int{}
+		for _, s := range strings.Split(temp, ",") {
+			index, err := strconv.Atoi(s)
+			if err != nil {
+				panic(err)
+			}
+			button = append(button, index)
+		}
+		buttons = append(buttons, button)
+	}
+	config = config[1 : len(config)-1]
+
+	return Machine{
+		Config:              strings.Split(config, ""),
+		JoltageRequirements: joltages,
+		Buttons:             buttons,
+	}
+}
+
+type Machine struct {
+	Config              []string
+	Buttons             [][]int
+	JoltageRequirements []int
+}
