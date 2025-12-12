6dd6eef53ee244f532a775e6d1657fc447a778c2
Wim Spaargaren <wim_spaargaren@live.nl>
2025-12-12 08:35:19 +0100

    day 12 2025 wip

--- 2025/day12/main.go
+++ 2025/day12/main.go
@@ -1,8 +1,195 @@
 package main
 
-import "github.com/wimspaargaren/aoc"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/wimspaargaren/aoc"
+)
 
 func main() {
 	input := aoc.MustReadFile("input.txt")
 	_ = input
+	grids, presents := parseInput(input)
+	for _, grid := range grids {
+		totalArea := 0
+		for i, presentID := range grid.Presents {
+			totalArea += presents[i].Area * presentID
+		}
+		if totalArea <= grid.Width*grid.Height {
+			// more checks needed here
+			// each shape has 4 rotations (i think we also need to consider flips?)
+			// each shape's rotation can be placed at any position in the grid
+			// check if all shapes can fit without overlapping
+			// this is a complex problem, for now we will just print YES if area fits
+			fmt.Println("YES")
+			fmt.Println(grid.Width*grid.Height-totalArea, "space left")
+			// create a list of indexes we need to add
+			ids := []int{}
+			for i, presentID := range grid.Presents {
+				for j := 0; j < presentID; j++ {
+					ids = append(ids, i)
+				}
+			}
+			fmt.Println("Presents to place:", ids)
+		} else {
+			fmt.Println("NO")
+		}
+	}
+}
+
+type Present struct {
+	Width    int
+	Height   int
+	Grid     [][]string
+	Variants [][][]string
+	Area     int
+}
+
+type Grid struct {
+	Width    int
+	Height   int
+	Presents []int
+}
+
+func parseInput(input string) ([]Grid, []Present) {
+	grids, presents := []Grid{}, []Present{}
+
+	isGrids := false
+	present := Present{}
+	for _, line := range strings.Split(input, "\n") {
+		if strings.Contains(line, "x") {
+			splitted := strings.Split(line, " ")
+			dimension := splitted[0]
+			dimensions := strings.Split(dimension[0:len(dimension)-1], "x")
+			fmt.Println(dimensions)
+			width, err := strconv.Atoi(dimensions[0])
+			if err != nil {
+				panic(err)
+			}
+			height, err := strconv.Atoi(dimensions[1])
+			if err != nil {
+				panic(err)
+			}
+			presentIDs := []int{}
+			for _, idStr := range splitted[1:] {
+				id, err := strconv.Atoi(idStr)
+				if err != nil {
+					panic(err)
+				}
+				presentIDs = append(presentIDs, id)
+			}
+			grid := Grid{
+				Width:    width,
+				Height:   height,
+				Presents: presentIDs,
+			}
+			grids = append(grids, grid)
+			isGrids = true
+			continue
+		}
+		if line == "" {
+			presents = append(presents, present)
+		}
+		if strings.HasSuffix(line, ":") {
+			present = Present{}
+		} else if !isGrids {
+			presentRow := strings.Split(line, "")
+			present.Grid = append(present.Grid, presentRow)
+			present.Height++
+			present.Width = len(presentRow)
+		}
+
+	}
+	for i, present := range presents {
+		// create variants (rotations) & flips
+		variants := [][][]string{}
+		variants = append(variants, present.Grid)
+		degree90 := rotate90(present.Grid)
+		variants = append(variants, degree90)
+		degree180 := rotate90(degree90)
+		variants = append(variants, degree180)
+		degree270 := rotate90(degree180)
+		variants = append(variants, degree270)
+		flipped := flip(present.Grid)
+		variants = append(variants, flipped)
+		flipped90 := rotate90(flipped)
+		variants = append(variants, flipped90)
+		flipped180 := rotate90(flipped90)
+		variants = append(variants, flipped180)
+		flipped270 := rotate90(flipped180)
+		variants = append(variants, flipped270)
+		unique := map[string]bool{}
+
+		for _, v := range variants {
+			_, exists := unique[gridToString(v)]
+			if !exists {
+				unique[gridToString(v)] = true
+				presents[i].Variants = append(presents[i].Variants, v)
+			}
+		}
+
+		// area is always the same
+		for _, row := range present.Grid {
+			for _, cell := range row {
+				if cell == "#" {
+					presents[i].Area++
+				}
+			}
+		}
+	}
+	for _, p := range presents {
+		fmt.Println(p)
+		for _, v := range p.Variants {
+			printGrid(v)
+			fmt.Println()
+		}
+	}
+	for _, g := range grids {
+		fmt.Println(g)
+	}
+	return grids, presents
+}
+
+func rotate90(grid [][]string) [][]string {
+	height := len(grid)
+	width := len(grid[0])
+	newGrid := make([][]string, width)
+	for i := range newGrid {
+		newGrid[i] = make([]string, height)
+	}
+	for r := 0; r < height; r++ {
+		for c := 0; c < width; c++ {
+			newGrid[c][height-1-r] = grid[r][c]
+		}
+	}
+	return newGrid
+}
+
+func flip(grid [][]string) [][]string {
+	height := len(grid)
+	width := len(grid[0])
+	newGrid := make([][]string, height)
+	for i := range newGrid {
+		newGrid[i] = make([]string, width)
+	}
+	for r := 0; r < height; r++ {
+		for c := 0; c < width; c++ {
+			newGrid[r][width-1-c] = grid[r][c]
+		}
+	}
+	return newGrid
+}
+
+func gridToString(grid [][]string) string {
+	res := ""
+	for _, row := range grid {
+		res += strings.Join(row, "") + "\n"
+	}
+	return res
+}
+
+func printGrid(grid [][]string) {
+	fmt.Println(gridToString(grid))
 }
